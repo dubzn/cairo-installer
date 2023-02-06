8b6afbd9fc8d8ded371517ae1fedcfb8a425a1a6
dpinones <pinonesdamian@gmail.com>
2023-02-06 17:35:22 -0300

    cairo installer v0.1

--- crates/cairo-lang-defs/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Representation and queries for definitions of module-level Cairo language elements.
-//! For example, resolving identifiers in the module level is done here.
-
-pub mod db;
-pub mod diagnostic_utils;
-pub mod ids;
-pub mod plugin;
-#[cfg(test)]
-mod test;
--- crates/cairo-lang-semantic/src/expr/mod.rs
+++
@@ -1,9 +0,0 @@
-//! This module is responsible for inner code elements, such as expressions and statements.
-#[cfg(test)]
-mod test;
-
-pub mod compute;
-pub mod fmt;
-pub mod inference;
-pub mod objects;
-pub mod pattern;
