72d89fae2afd6ac4b777b052b8ae358251da971c
Santiago Galván (Dub) <santiagodgalvan@gmail.com>
2023-02-11 20:10:19 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -104,7 +104,7 @@ main() {
     ############################################################################
     ${NC}\\n"
 
-    version_is_supported=$(set_cairo_version $1)
+    version_is_supported=$(set_cairo_version)
     printf "[main] version_is_supported=$version_is_supported \\n"
     set_bash_file
     terminal_supported=$?
