5aeab88a0db0615d052d5ca98261d18d860faf9c
Santiago Galván (Dub) <santiagodgalvan@gmail.com>
2023-02-11 20:38:44 -0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -91,6 +91,7 @@ clean() {
 run_cairo_version() {
     if ! command -V "cairo-compile" > /dev/null 2>&1; then
         printf "${BGreen}[!] Cairo installation was successful! (v$CAIRO_VERSION)${NC}\\n"
+        cairo-compile --version
         printf "${BPurple}\\n[!] Trying to run Hello World..${NC}\\n"
         cairo-run -p ./src/hello_world.cairo         
     else 
