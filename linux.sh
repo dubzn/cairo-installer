ce8c15b352ce5aca635e7bef0d5d6d0d3a989454
Santiago Galván (Dub) <santiagodgalvan@gmail.com>
2023-02-11 21:10:30 -0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -94,8 +94,10 @@ run_cairo_version() {
         printf "${BGreen}[!] Cairo installation was successful! (v$CAIRO_VERSION)${NC}\\n"
         printf "${BPurple}\\n[!] Trying to run Hello World..${NC}\\n"
         $HOME/cairo/bin/cairo-compile --version
+        echo "PATH: $PATH"
         export PATH="$CAIRO_ENV1"
         echo "PATH: $PATH"
+        cairo-compile --version
     else 
         printf "${BRed}[!] Cairo installation failed!${NC}\\n"
     fi
