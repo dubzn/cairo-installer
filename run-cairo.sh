09aa0256459725fcf7e92f5bb306a2e6e85e0128
dubzn <santiagodgalvan@gmail.com>
2023-03-07 00:45:00 -0300

    adding cairo_path to hello world

--- run-cairo.sh
+++ run-cairo.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
 BPurple='\033[1;35m'      # Purple
 NC='\033[0m'              # Text Reset
 
 run_cairo() {
-    if ! command "-h" "cairo-compile" > /dev/null 2>&1; then
-        printf "${BPurple}[!] Trying to run Hello World..${NC}\\n"
-        echo $BASH_FILE
-        source $BASH_FILE 2> /dev/null || true
+    printf "${BPurple}[!] Trying to run Hello World..${NC}\\n"
+    printf "${BPurple}[!] You may need to run 'source $BASH_FILE' (or restart console) for the changes to take effect${NC}\\n"
+    if [ ! -z "$1" ]; then
+        $1/cairo-compile --version
+        $1/cairo-run -p ./src/hello_world.cairo               
+        return 
+    else ! command "-h" "cairo-compile" > /dev/null 2>&1; then
         cairo-compile --version
         cairo-run -p ./src/hello_world.cairo               
-    else 
-        printf "${BPurple}[!] You may need to run 'source $BASH_FILE' (or restart console) for the changes to take effect${NC}\\n"
-    fi
+   fi
 }
 
-run_cairo
+run_cairo $1
