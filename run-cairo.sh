3a870c84b6979b76b4def13ed03f848cd6fbc18c
dubzn <santiagodgalvan@gmail.com>
2023-03-07 01:14:37 -0300

    remove log for bash file

--- run-cairo.sh
+++ run-cairo.sh
@@ -5,11 +5,10 @@ NC='\033[0m'              # Text Reset
 
 run_cairo() {
     printf "${BPurple}[!] Trying to run Hello World..${NC}\\n"
-    printf "${BPurple}[!] You may need to run 'source $BASH_FILE' (or restart console) for the changes to take effect${NC}\\n"
     if [ ! -z "$1" ]; then
         $1/cairo-compile --version
         $1/cairo-run -p ./src/hello_world.cairo               
-    else ! command "-h" "cairo-compile" > /dev/null 2>&1;
+    else 
         cairo-compile --version
         cairo-run -p ./src/hello_world.cairo               
     fi
